05e707c0ff786728cf3f30eac611a1a655e4f3b5
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-20 20:27:43 -0300

    Configuração inicial do Terraform4

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -15,7 +15,7 @@ terraform {
   }
 }
 
-provider "aws" {
+#provider "aws" {
   region                   = "us-east-1"
   shared_config_files      = ["~/.aws/config"]
   shared_credentials_files = ["~/.aws/credentials"]
