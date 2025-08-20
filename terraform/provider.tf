6b1191a8e76b821dad6187d96ea1b9c884e3c4e1
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-20 20:19:22 -0300

    Configuração inicial do Terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,6 @@
 # PROVIDER
 terraform {
-
   required_version = "~> 1.12.2"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -10,11 +8,16 @@ terraform {
     }
   }
 
+  backend "s3" {
+    bucket = "aws-s3-tfstate-lamata"
+    key    = "tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
   region                   = "us-east-1"
-  shared_config_files      = [".aws/config"]
-  shared_credentials_files = [".aws/credentials"]
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "iac"
 }
