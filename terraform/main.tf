673530e8139979a68045a1e2390fc36da07aa1a8
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-17 20:55:58 -0300

    fix: terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 module "network" {
-  source      = "./modules/network"
-  vpc_cidr    = "10.0.0.0/16"
-  subnet_cidr = "10.0.1.0/24"
-  az          = "us-east-1a"
+  source           = "./modules/network"
+  vpc_cidr         = "10.0.0.0/16"
+  subnet_cidr      = "10.0.1.0/24"
+  az               = "us-east-1a"
   allowed_ssh_cidr = ["0.0.0.0/0"] # Troque pelo seu IP
 }
 
